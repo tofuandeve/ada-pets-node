b4199e993e8e6cfbd302e529643446bde2ad02a2
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-11-13 16:01:21 -0800

    Reorganize readline things in main.js

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const axios = require('axios');
 const readline = require('readline');
 
+// Setup for the readline library.
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+reader.setPrompt(`Options:\n${Object.keys(options).join("\n  ")}\n\nWhat would you like to do? `);
+
+reader.prompt();
+
+// Option functions.
 const exit = () => {
   console.log("Thank you for using the Ada Pets Adoption App!");
   process.exit();
 }
 
+// Register the options.
 const options = {
   "exit": exit
 }
 
-reader.setPrompt(`Options:
-  ${Object.keys(options).join("\n  ")}
-
-What would you like to do? `);
-
-reader.prompt();
-
+// Code to display the menu and prompt then run the selected option.
 reader.on('line', function(choice) {
   const selected = options[choice.trim().toLowerCase()];
 
