c6cd661cd38f29c618c8aac4d3543ee6d1075c75
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-11-17 16:56:22 -0800

    Add Wave 3 tests

---
+++ test/wave3.test.js
@@ -0,0 +1,77 @@
+const axios = require('axios');                    // Import axios.
+const MockAdapter = require('axios-mock-adapter'); // This is kind of like VCR.
+const result = require('../src/result.js');        // Import result handling.
+const setHandlers = result.setHandlers;
+
+//b Import our function(s) for testing.
+const adaPets = require('../src/adaPets.js');
+const removePet = adaPets.removePet;
+
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(axios);
+
+const fail = (error) => {
+  throw new Error(`Test failed! ${error}`);
+}
+
+describe("Wave 1", () => {
+  // Set up axios test responses.
+  describe("listPets", () => {
+    it("Can show details for a pet", done => {
+      // Arrange.
+      // Set up what we want the API to return for this test.
+      mock.onDelete("https://petdibs.herokuapp.com/pets/3").reply(204);
+
+      // Assertions come first because they need to be ready before the function call.
+      setHandlers(
+        result => {
+          setTimeout(() => {    // We need this to consistently display assertion errors.
+            // No assertions.  We just care that it happened.
+            done();
+          })},
+        fail);
+
+      // Act.
+      removePet(3);
+    });
+
+    it("sets an error string when there is no selected pet", done => {
+      setHandlers(
+        fail,                   // Fail if we don't setError.
+        error => {
+          setTimeout(() => {    // We need this to consistently display assertion errors.
+            // Assert.
+            expect(error.constructor).toBe(String);
+            expect(error).toMatch("remove");
+            expect(error).toMatch("select");
+
+            done();
+          })}
+      );
+
+      // Act.
+      removePet();
+    })
+
+    it("sets an error string when the response isn't successful", done => {
+      // Arrange.
+      // We want this to fail.
+      mock.onDelete("https://petdibs.herokuapp.com/pets/1000000").reply(404);
+
+      setHandlers(
+        fail,                   // Fail if we don't setError.
+        error => {
+          setTimeout(() => {    // We need this to consistently display assertion errors.
+            // Assert.
+            expect(error).toMatch(/failed/i);
+            expect(error).toMatch(/remove/i);
+
+            done();
+          })}
+      );
+
+      // Act.
+      removePet(1000000);
+    });
+  });
+});
